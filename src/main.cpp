41b86819d75df362e5ff5d8369d678c139dc5229
Andrés Morales <amoralesma@unal.edu.co>
2025-03-20 09:15:38 -0500

    [Update] gitignore, basic structure

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file main.cpp
+ * @author Andrés Morales (amoralesma@unal.edu.co)
+ * @brief 
+ * @version 0.1
+ * @date 2025-03-20
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <iostream>
+
+ int main(int argc, char* argv[]){
+    std::cout << "Hola mundo" << std::endl; 
+
+    
+
+    return 1; 
+ }
